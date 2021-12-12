430e90ca82f34aa3e8d4433f83ea02154f9845b4
Bevelman <91498634+Bevelman@users.noreply.github.com>
2021-12-12 15:53:40 +0000

    dgad

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from signal import signal, SIGTERM, SIGHUP, pause
-from rpi_lcd import LCD
-lcd = LCD()
-def safe_exit(signum, frame):
-    exit(1)
-try:
-    signal(SIGTERM, safe_exit)
-    signal(SIGHUP, safe_exit)
-    lcd.text("Hello,", 1)
-    lcd.text("Raspberry Pi!", 2)
-    pause()
-except KeyboardInterrupt:
-    pass
-finally:
-    lcd.clear()
+# Import LCD library
+from RPLCD import i2c
+
+# Import sleep library
+from time import sleep
+
+# constants to initialise the LCD
+lcdmode = 'i2c'
+cols = 16
+rows = 2
+charmap = 'A00'
+i2c_expander = 'PCF8574'
+
+# Generally 27 is the address;Find yours using: i2cdetect -y 1 
+address = 0x27 
+port = 1 # 0 on an older Raspberry Pi
+
+# Initialise the LCD
+lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
+                  cols=cols, rows=rows)
+
+# Write a string on first line and move to next line
+lcd.write_string('Hello world')
+lcd.crlf()
+lcd.write_string('IoT with Vincy')
+lcd.crlf()
+lcd.write_string('Phppot')
+sleep(5)
+# Switch off backlight
+lcd.backlight_enabled = False 
+# Clear the LCD screen
+lcd.close(clear=True)
