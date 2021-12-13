00dbb9d64f95bcc4ecd254e5804e4c2693c10391
Bevelman <91498634+Bevelman@users.noreply.github.com>
2021-12-13 15:43:01 +0000

    added launch sequence

--- shooter.py
+++ shooter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python37all
+
 import cgi, json
 
 data = cgi.FieldStorage()         
@@ -6,9 +7,13 @@ data = cgi.FieldStorage()
 # set up new data from POST:
 slider_val1 = data.getvalue('slider1')
 slider_val2 = data.getvalue('slider2')
+if data.getvalue('launch')=='Launch!':
+  launched = 1
+else:
+  launched = 0
 
 # form new dictionary with user data:
-fileData = {'slider1':slider_val1, 'slider2':slider_val2}
+fileData = {'slider1':slider_val1, 'slider2':slider_val2, 'launch':launched}
 
 # write all data to the file:
 with open('shooter.txt', 'w') as f:
@@ -18,9 +23,10 @@ with open('shooter.txt', 'w') as f:
 print("Content-type: text/html\n\n")
 print('<html>')
 print('<body>')
-print("s1 = " + data.getvalue('slider1') + '<br>')
-print("s2 = " + data.getvalue('slider2') + '<br>')
-print('<input type="submit" value="Submit">')
+print('<input type="range" name="slider1" min ="3" max="12" value ="%s"><br>' % slider_val1)
+print('<input type="range" name="slider2" min ="3" max="12" value ="%s"><br>' % slider_val2)
+print('<input type="submit" name="angle" value="Set angle">')
+print('<input type="submit name="launch" value="Launch!">')
 print('</form>')
 print('</body>')
 print('</html>')
--- shooter_bckgnd.py
+++ shooter_bckgnd.py
@@ -4,39 +4,58 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-pwmPin1 = 20
-pwmPin2 = 21
-GPIO.setup(pwmPin1, GPIO.OUT)
-GPIO.setup(pwmPin2, GPIO.OUT)
+lArmPin = 16
+xRotPin = 20
+yRotPin = 21
+leftMPin = 19
+#rightMPin = 26
+GPIO.setup(xRotPin, GPIO.OUT)
+GPIO.setup(yRotPin, GPIO.OUT)
+GPIO.setup(lArmPin, GPIO.OUT)
+GPIO.setup(leftMPin, GPIO.OUT)
+#GPIO.setup(rightMPin, GPIO.OUT)
 
-# set min & max % duty cycles (5 and 10 are default values, but play
-# around to find optimum values for your motor)
+# set min & max % duty cycles for servos
 dcMin = 3
 dcMax = 12
 
-pwm1 = GPIO.PWM(pwmPin1, 50) # PWM object at 50 Hz (20 ms period)
-pwm2 = GPIO.PWM(pwmPin2, 50)
-pwm1.start(0)
-pwm1.start(0)
+pwmx = GPIO.PWM(xRotPin, 50) # controls x rotation servo
+pwmy = GPIO.PWM(yRotPin, 50) # controls y rotation servo
+pwmL = GPIO.PWM(lArmPin, 50) # controls launch arm servo
+pwmM = GPIO.PWM(leftMPin, 50) # controls flywheels
+pwmx.start(0)
+pwmy.start(0)
+pwmL.start(0)
+#initialize to halfway to hold projectile in place
+pwmL.ChangeDutyCycle(7)
+
 
 try:
   while True:
     # The next 3 lines are just in case file is empty or can't be opened:
     slider1_val = '3'
     slider2_val = '3'
-    data = json.loads("'slider1':slider1_val, 'slider2':slider2_val")  
+    data = json.loads("'slider1':slider1_val, 'slider2':slider2_val, 'launch':0")  
 
     # Get data from the file:
     with open("led-pwm-multiple.txt", 'r') as f:
       data = json.load(f)
 
-    # Change the DC for the given LED:
+    # Rotate the servos based on slider input
     slider1_val = int(data['slider1'])
     slider2_val = int(data['slider2'])
-    pwm1.ChangeDutyCycle(slider1_val)
-    pwm2.ChangeDutyCycle(slider2_val)
-
+    pwmx.ChangeDutyCycle(slider1_val)
+    pwmy.ChangeDutyCycle(slider2_val)
+    # Run launch sequence if launch button is pressed
+    if bool(data['launch']):
+      pwmM.ChangeDutyCycle(100) #run flywheels
+      time.sleep(0.5)
+      pwmL.ChangeDutyCycle(3) #load projectile
+      time.sleep(0.5)
+      pwmL.ChangeDutyCycle(12) #push projectile into flywheels
     time.sleep(0.1)  # small sleep step to avoid re-opening the file too often
+    
+
 except KeyboardInterrupt:
   print("closing")
   GPIO.cleanup()
